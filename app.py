df957055b34338acfdcb2a0ff467e171f4fcc2dd
gael <cgvalenzuelac@concredito.com.mx>
2021-04-05 17:26:40 -0600

    templates .

--- app.py
+++ app.py
@@ -1,26 +1,29 @@
-from flask import Flask, render_template
-import json
-
-from data import lista_invitados, agregar_invitado
+from flask import Flask, render_template, request, redirect
+from data import lista_invitados, agregar_invitado, confirmar_invitacion
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    return json.dumps(lista_invitados)
+    return render_template('lista.html', lista=lista_invitados)
 
 
-@app.route('/add/<nombre>/')
-def agregar(nombre=''):
-    agregar_invitado(nombre)
-    return 'listo {nombre}'.format(nombre=nombre)
+@app.route('/add/', methods=['GET', 'POST'])
+def agregar():
+    if request.method == 'GET':
+        return render_template('agregar.html')
+    else:
+        nombre = request.form.get("nombre")
+        agregar_invitado(nombre)
+        return redirect('/')
 
 
-@app.route('/about')
-def about():
-    nombre = 'gael'
-    return render_template('about.html', nombre=nombre)
+@app.route('/confirmar/<nombre>')
+def confirmar(nombre=''):
+    print(nombre)
+    confirmar_invitacion(nombre)
+    return redirect('/')
 
 
 if __name__ == "__main__":
