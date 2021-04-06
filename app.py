a4d90bc6eca8126a4c9bc2345ad0f8934278b4cc
gael <cgvalenzuelac@concredito.com.mx>
2021-04-05 19:30:21 -0600

    mejorar layout.

--- app.py
+++ app.py
@@ -6,11 +6,23 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    return redirect('/invitados')
+
+
+@app.route('/invitados')
+def invitados():
+    ###
+    # mustra el listado de invitados.
+    ###
     return render_template('lista.html', lista=lista_invitados)
 
 
 @app.route('/add/', methods=['GET', 'POST'])
 def agregar():
+    ###
+    # en el metodo get muestra la pagina de el formularui para agregar.
+    # agregar invitado ingresa el nombre en el listado llamando la funcion de agregar
+    ###
     if request.method == 'GET':
         return render_template('agregar.html')
     else:
@@ -21,10 +33,16 @@ def agregar():
 
 @app.route('/confirmar/<nombre>')
 def confirmar(nombre=''):
-    print(nombre)
+    ###
+    # Este metodo solo cambia el estado de confirmacion mediante el nombre.
+    # y redirecciona a la pagina principal.
+    ###
     confirmar_invitacion(nombre)
     return redirect('/')
 
 
+###
+# inicia la aplicacion web si el app esta en el main.
+###
 if __name__ == "__main__":
     app.run(debug=True, port=8000)
