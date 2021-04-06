1017aa1916790cb74b87b35e07d62a2ad660e931
gael <cgvalenzuelac@concredito.com.mx>
2021-04-06 15:28:49 -0600

    nombre bd.

--- config/mongoDB.py
+++ config/mongoDB.py
@@ -1,19 +1,11 @@
 from mongoengine import connect
 
-DB = 'base'
-HOST = 'mongodb+srv://admin:1234@cluster0.4geja.mongodb.net/base'
-HOST_2 = 'mongodb+srv://admin:1234@cluster0.4geja.mongodb.net/base?retryWrites=true&w=majority'
-LOCALHOST = "mongodb://127.0.0.1:27017/"
-PORT = 27017
-# Conexion base
-# connect(host=HOST_2)
-
 
 def init_mongo_db():
     connect(
-        db='base',
+        db='invitatos_web',
         username='admin',
         password='1234',
-        host='mongodb+srv://admin:1234@cluster0.4geja.mongodb.net/base',
+        host='mongodb+srv://admin:1234@cluster0.4geja.mongodb.net',
         alias='db_base'
     )
