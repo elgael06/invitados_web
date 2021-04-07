bb126176981cd2f5a4fbb9e85f068caec789b1b8
gael <cgvalenzuelac@concredito.com.mx>
2021-04-07 13:13:17 -0600

    password.

--- config/mongoDB.py
+++ config/mongoDB.py
@@ -4,8 +4,8 @@ from mongoengine import connect
 def init_mongo_db():
     connect(
         db='invitatos_web',
-        # username='admin',
-        # password='1234',
+        username='admin',
+        password='1234',
         host='mongodb+srv://admin:1234@cluster0.4geja.mongodb.net',
         # 'mongodb://localhost:27017',
         alias='db_base'
